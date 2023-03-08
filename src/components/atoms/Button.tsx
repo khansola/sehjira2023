9d01f01257bc9e4b221938a645f841710d737831
alvitoZ <alvitozidane76@gmail.com>
2023-03-08 13:25:04 +0700

    coba fork

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,10 +1,28 @@
 type props = {
-	children?: React.ReactNode;
-	title?: string;
-  action: (e:  React.MouseEvent) => void;
+  children?: React.ReactNode;
+  title?: string;
+  caption?: string;
+  action: (e: React.MouseEvent) => void;
 };
 const Button = ({ children, title, action }: props) => {
-	return title === "" ? <button onClick={(e) => {action(e)}}>{children}</button> : <button className="py-2 px-5 bg-violet-700 flex items-center rounded-md font-semibold popup" onClick={(e) => {action(e)}}>{title}</button>;
+  return title === "" ? (
+    <button
+      onClick={(e) => {
+        action(e);
+      }}
+    >
+      {children}
+    </button>
+  ) : (
+    <button
+      className="py-2 px-5 bg-violet-700 flex items-center rounded-md font-semibold popup"
+      onClick={(e) => {
+        action(e);
+      }}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default Button;
